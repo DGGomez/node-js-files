4e633e6ebddf0ffca2f2c0c95c7c733d53caacaf
Daniel Gomez <dggomez21@gmail.com>
2019-03-06 08:45:58 -0500

    Create dataController.js

---
+++ basic/controllers/dataController.js
@@ -0,0 +1,19 @@
+exports.list_all = function(req, res) {
+  return "list_all success";
+};
+
+exports.create = function(req, res) {
+    return "create success";
+};
+
+exports.read = function(req, res) {
+     return "read success";
+};
+
+exports.update = function(req, res) {
+     return "update success";
+};
+
+exports.delete = function(req, res) {
+  return "delete success";
+};
